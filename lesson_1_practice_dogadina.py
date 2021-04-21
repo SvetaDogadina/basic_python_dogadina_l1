2e7d63d5fab2f2bd48db7a1a7039b8a2966dfed7
SvetaDogadina <82947780+SvetaDogadina@users.noreply.github.com>
2021-04-21 15:01:44 +0300

    Initial commit

---
+++ lesson_1_practice_dogadina.py
@@ -0,0 +1,18 @@
+# Урок 1 Практика Догадина Светлана
+# Задание 1
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран
+# Ответ: создадим программу - шутку "Dumb 64bit"
+name = input("Enter your name: ")
+print("Hi, " + name)
+year_of_birth = int(input("Enter your year of birth in YYYY format: "))
+age = str(2021 - year_of_birth)
+print("So you are " + age + " years old?")
+answer = input("Type answer YES or NO: ")
+if answer == "YES":
+    print("Cool! My 64bit works good today")
+    access = True
+if answer == "NO":
+    print("You are drunk! Go home")
+    access = False
