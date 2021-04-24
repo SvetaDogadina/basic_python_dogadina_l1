efa043f59623e5528c6c8c45df9201218ab4df2f
SvetaDogadina <fotisd2013@gmail.com>
2021-04-24 11:06:56 +0300

    Initial commit

---
+++ lesson_1_practice_task_4_dogadina.py
@@ -0,0 +1,15 @@
+# Урок 1 Практика Догадина Светлана
+# Задание 4
+# Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+x = int(input("Enter any positive integer:"))
+y = x%10
+x = x//10
+#print(y)
+#print(x)
+while x > 0:
+    if x % 10>y:
+        y = x%10
+    x = x//10
+    #print(x, x % 10)
+print("The biggest number in your integer is ",y)
