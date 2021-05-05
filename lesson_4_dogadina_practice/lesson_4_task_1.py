0b02a9156b9b0dfcd46a4ea5ed1aebd9d7dfbe9b
SvetaDogadina <fotisd2013@gmail.com>
2021-05-05 20:51:31 +0300

    Initial commit

--- lesson_4_dogadina_practice/lesson_4_task_1.py
+++ lesson_4_dogadina_practice/lesson_4_task_1.py
@@ -4,9 +4,13 @@
 # Для выполнения расчета для конкретных
 # значений необходимо запускать скрипт с параметрами
 from sys import argv
-salary_per_hour, hours, bonus = argv
+address, salary_per_hour, hours, bonus = argv
+print("Путь: ", address)
 print("Ставка в час: ", salary_per_hour)
 print("Часы: ", hours)
 print("Премия: ", bonus)
+salary_per_hour = int(salary_per_hour)
+hours = int(hours)
+bonus = int(bonus)
 salary = ((salary_per_hour * hours) + bonus)
 print("Расчет заработной платы: ", salary)
