9c96e6f53d90426503b6cc6d58fbafb981763779
SvetaDogadina <fotisd2013@gmail.com>
2021-05-10 20:10:35 +0300

    Initial commit

---
+++ lesson5/task_5.py
@@ -0,0 +1,8 @@
+def summary():
+    try:
+        with open('5_task', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
