4a3d73283d44a6ba55792d6b5992a04802adec7e
SvetaDogadina <82947780+SvetaDogadina@users.noreply.github.com>
2021-05-10 20:22:49 +0300

    Delete practice_drafts.py

--- practice_drafts.py
+++
@@ -1,8 +0,0 @@
-with open("45566") as file_obj:
-    lines = 0
-    letters = 0
-    for line in file_obj:
-        lines += line.count("\n")
-        letters = len(line)-1
-        print(f"{letters} letters in line")
-    print(f"String count is {lines}")
